82f924cf7febc9d2f5322a62d2b2ec63f9514ec1
Philipp Muens <raute1337@gmx.de>
2016-09-10 16:43:22 +0200

    Create todos-delete.js

---
+++ xx-code/todos/backend/todos-delete.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports = (event, callback) => {
+  const params = {
+    TableName : 'todos',
+    Key: {
+      id: event.path.id
+    }
+  };
+
+  return dynamoDb.delete(params, function (error, data) {
+    if (error) {
+      callback(error);
+    } else {
+      callback(error, params.Key);
+    }
+  });
+};
