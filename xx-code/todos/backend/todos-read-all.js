66c6b9fe5f7abccb037744db930164d905c74550
Philipp Muens <raute1337@gmx.de>
2016-09-10 16:43:34 +0200

    Create todos-read-all.js

---
+++ xx-code/todos/backend/todos-read-all.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports = (event, callback) => {
+  const params = {
+    TableName: 'todos',
+  };
+
+  return dynamoDb.scan(params, (error, data) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(error, data.Items);
+    }
+  });
+};
