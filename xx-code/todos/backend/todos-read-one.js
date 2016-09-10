824c6e0efbb2b3f3ff217e75691122dd81b3d39d
Philipp Muens <raute1337@gmx.de>
2016-09-10 16:43:44 +0200

    Create todos-read-one.js

---
+++ xx-code/todos/backend/todos-read-one.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports = (event, callback) => {
+  const params = {
+    TableName: 'todos',
+    Key: {
+      id: event.path.id
+    }
+  };
+
+  return dynamoDb.get(params, (error, data) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(error, data.Item);
+    }
+  });
+};
